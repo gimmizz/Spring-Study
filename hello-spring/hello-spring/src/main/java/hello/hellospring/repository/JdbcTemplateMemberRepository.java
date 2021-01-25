1bffd26371b9bbf7f260fa7361e342f6cc6e316d
gimmizz <69383392+gimmizz@users.noreply.github.com>
2021-01-25 22:45:07 +0900

    JdbcTemplate까지 성공

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,77 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    // Datasource injection이 필요하다.
+
+    @Autowired // 생성자가 단 하나만 있을 경우, Autowired를 생략할 수도 있다.
+    public JdbcTemplateMemberRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName()); // 위의 4줄을 통해 INSERT 구문을 만들어줄수 있다.
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters)); // key를 받고
+        member.setId(key.longValue()); // key를 통해서 id를 만든다!
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) { // [Ctrl]+[Alt]+[V] 로 꺼낸다.
+        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(), id);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from member", memberRowMapper());
+    }
+
+    private RowMapper<Member> memberRowMapper(){
+//        return new RowMapper<Member>() {
+//            @Override
+//            public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                Member member = new Member();
+//                member.setId(rs.getLong("id"));
+//                member.setName(rs.getString("name"));
+//                return member;
+//            }
+//        } -> 이걸 람다로 바꾼게 아래 [Alt]+[Enter]
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
+    }
+}
