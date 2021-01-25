6f2745ca2249689f6029247c18d1091df19d25f2
gimmizz <69383392+gimmizz@users.noreply.github.com>
2021-01-25 21:32:01 +0900

    순수 JDBC 까지 성공

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource; // spring에서 자체적으로 dataSource를 제공. springConfig에 주입.
+    }
+
     // 동작 순서
     // 1. @Bean 2개 인식해서 각각 스프링 빈으로 등록
     // 2. memberService 생성 시, 스프링 빈으로 등록되어있는 memberRepository을 넣어서 생성
@@ -18,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- hello-spring/hello-spring/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -1,4 +1,146 @@
 package hello.hellospring.repository;
+import hello.hellospring.domain.Member;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-public class JdbcMemberRepository {
+public class JdbcMemberRepository implements MemberRepository {
+    private final DataSource dataSource; // DB에 붙으려면 datasource가 필요
+    public JdbcMemberRepository(DataSource dataSource) { // 스프링을 통해 datasource를 주입 받는다.
+        this.dataSource = dataSource;
+    }
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(name) values(?)"; // ? : parameter binding
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null; // 결과를 받는 것
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql,
+                    Statement.RETURN_GENERATED_KEYS); // RETURN_GENERATED_KEYS : DB에 INSERT를 해야 KEY 값을 얻었었던 것!
+            pstmt.setString(1, member.getName());
+            pstmt.executeUpdate(); // DB에 쿼리를 날린다!
+            rs = pstmt.getGeneratedKeys(); // RETURN_GENERATED_KEYS과 매칭되어서 할 수 있는 것! KEY를 꺼낸다.
+            if (rs.next()) {
+                member.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+            return member;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs); // 자원을 release해야 한다. resouce 반환.
+        }
+    }
+    @Override
+    public Optional<Member> findById(Long id) {
+        String sql = "select * from member where id = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, id);
+            rs = pstmt.executeQuery(); // 조회니까 executeUpdate가 아니라 Query
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+    @Override
+    public List<Member> findAll() {
+        String sql = "select * from member";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            List<Member> members = new ArrayList<>(); // 여러 개니까!
+            while(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                members.add(member);
+            }
+            return members;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+    @Override
+    public Optional<Member> findByName(String name) {
+        String sql = "select * from member where name = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+            rs = pstmt.executeQuery();
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            }
+            return Optional.empty();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+    private Connection getConnection() {
+        // 할때마다 새로운 connection을 쓰는 게 아니라, 한번만 하는 이유:
+        // 스프링 프레임워크를 통해서 getConnection을 쓸 때는, DataSourceUtils를 통해서 datasource를 획득해야 한다.
+        // 그래야, 이전의 db connection가 유지를 할 수 있다.
+        return DataSourceUtils.getConnection(dataSource);
+    }
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs)
+    {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource); // 닫을 때도 반드시 DataSourceUtils를 통해서 release해야.
+    }
 }
